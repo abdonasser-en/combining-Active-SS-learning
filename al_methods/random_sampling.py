645d2a57ed7f790f7208b1f1ce9ba8af6fa57a86
abdonasser-en <abdonasser445@gmail.com>
2023-08-11 16:24:09 +0200

    al_methodes updated

--- al_methods/random_sampling.py
+++ al_methods/random_sampling.py
@@ -1,11 +1,12 @@
 import numpy as np
-from ..strategy import Strategy
+from strategy_utils_framework.strategy import Strategy
 import pdb
 
 class RandomSampling(Strategy):
     def __init__(self, X, Y, X_te, Y_te, idxs_lb, net, handler, args):
         super(RandomSampling, self).__init__(X, Y, X_te, Y_te, idxs_lb, net, handler, args)
 
-    def query(self, n):
-        inds = np.where(self.idxs_lb==0)[0]
-        return inds[np.random.permutation(len(inds))][:n]
+def query(self, n):
+    inds = np.where(self.idxs_lb==0)[0]
+    return inds[np.random.permutation(len(inds))][:n]
+
