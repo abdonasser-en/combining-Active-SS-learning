49c2f4e28bfc5d94b1a616f3dff34fef9afb3ecb
abdonasser-en <abdonasser445@gmail.com>
2023-08-16 17:17:58 +0200

    framework updated

--- framework/framework2.py
+++ framework/framework2.py
@@ -2,10 +2,8 @@
 from strategy_utils_framework.strategy import Strategy
 
 class Framework2(Strategy):
-    def __init__(self,X, Y, X_te, Y_te, idxs_lb, net, handler, args,active_strategy, semi_supervised_strategy):
+    def __init__(self,X, Y, X_te, Y_te, idxs_lb, net, handler, args):
         super(Framework2, self).__init__(X, Y, X_te, Y_te, idxs_lb, net, handler, args)
-        self.active_strategy=active_strategy
-        self.strat2=semi_supervised_strategy
     
 
 
