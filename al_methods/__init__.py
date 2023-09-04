40704eabd1755827e55544885e9c672295994245
abdonasser-en <abdonasser445@gmail.com>
2023-09-04 17:24:39 +0200

    corset add to init

--- al_methods/__init__.py
+++ al_methods/__init__.py
@@ -2,6 +2,7 @@
 from .least_confidence import LeastConfidence
 from .batch_BALD import BatchBALD
 from .entropy_sampling import EntropySampling
+from .core_set import CoreSet
 # from .kmeans_sampling import KMeansSampling
 # from .kcenter_greedy import KCenterGreedy
 # # from .baseline_sampling  import BaselineSampling
