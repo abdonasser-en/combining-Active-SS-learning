5577575a2c2b724fd6960189a9216fce92c6141b
abdonasser-en <abdonasser445@gmail.com>
2023-09-11 14:02:29 +0200

    framework 2 updated

--- framework/framework2.py
+++ framework/framework2.py
@@ -21,6 +21,7 @@ from sklearn.metrics import pairwise_distances
 from torchmetrics import MatthewsCorrCoef
 from torchvision import transforms
 import pathlib
+from sklearn.metrics import cohen_kappa_score as c_k
 
 class Framework2(Strategy):
     def __init__(self,X, Y, X_te, Y_te, idxs_lb, net, handler, args):
@@ -71,4 +72,75 @@ class Framework2(Strategy):
         return np.argmax(predictions,axis=1)
     
 
-    transforms.ToTensor()
+
+    def train_framework(self,stratAl:object,stratSSL:object,NUM_ROUND,NUM_QUERY,alpha,n_epochs):
+
+        print(f' Sratgey for active learning{stratAl} and strategy for semi-supervised learning used {stratSSL}')
+        stratAl=stratAl(self.X_tr, self.Y_tr, self.X_te, self.Y_te, self.idxs_lb, self.net, self.handler, self.args,self.n_pool,self.device)
+        stratSSL=stratSSL(self.X_tr, self.Y_tr, self.X_te, self.Y_te, self.idxs_lb, self.net, self.handler, self.args,self.n_pool,self.device,self.predict,self.g)
+
+        # Train father
+        self.train(alpha=2e-3,n_epoch=5)
+
+
+        test_acc=self.predict(self.X_te,self.Y_te)
+        acc = np.zeros(NUM_ROUND+1)
+        acc[0] = test_acc
+        for rd in range(1, NUM_ROUND+1):
+
+            labeled = len(np.arange(self.n_pool)[self.idxs_lb])
+            if NUM_QUERY > int(self.args.nEnd*self.n_pool/100) - labeled:
+                NUM_QUERY = int(self.args.nEnd*self.n_pool/100) - labeled
+            
+        # query
+        ts = time.time()
+        output = stratAl.query(NUM_QUERY)
+        q_idxs = output
+        #predict father and son
+        prediction=self.predict_coefficient(self.X, self.Y,q_idxs)
+        prediction_w=self.predict_coefficient_w(self.X, self.Y,q_idxs)
+        print("father: ", prediction)
+        print("Son : ",prediction_w)
+        # compute the coefficient
+        cof=c_k(prediction,prediction_w)
+        print(cof)    
+        # update
+        self.idxs_lb[q_idxs] = True
+        te = time.time()
+        tp = te - ts
+        self.update(self.idxs_lb)
+
+        if cof<0.8:
+            # Al_methods
+            print( 'AL Methods')
+            print('Round {}/{}'.format(rd, NUM_ROUND), flush=True)
+
+            if hasattr(stratAl, 'train'):
+            
+                best_test_acc=stratAl.train(alpha=2e-3, n_epoch=5)
+            else: best_test_acc = self.train(alpha=2e-3, n_epoch=5)
+            print(best_test_acc)
+
+
+            t_iter = time.time() - ts
+            
+            # round accuracy
+            # test_acc = strategy.predict(X_te, Y_te)
+            acc[rd] = best_test_acc
+        else:
+            #SSL methods
+            print("SSL Methods")
+            print('Round {}/{}'.format(rd, NUM_ROUND), flush=True)
+            best_test_acc = stratSSL.train(alpha=2e-3, n_epoch=5)
+
+
+            t_iter = time.time() - ts
+            
+            # round accuracy
+            # test_acc = strategy.predict(X_te, Y_te)
+            acc[rd] = best_test_acc
+
+
+
+    
+
